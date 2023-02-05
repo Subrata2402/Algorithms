87b2c66b4b48dd025e9d25fbc5f086e2b17fd7eb
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2023-02-06 05:11:29 +0530

    update

--- All Pair Shortest Path/floyd_warshall.py
+++ All Pair Shortest Path/floyd_warshall.py
@@ -1,5 +1,29 @@
-# Floyd-Warshall Algorithm
+# All Pair Shortest Path Program (Floyd-Warshall)
+from math import inf
 
-Procedure floyd_warshall(weight):
-	for i in range(-1, n):
-		for j in range(-1, n):
+def all_pair_shortest_path(dist: list[list]) -> None:
+	"""Find the all pair shortest path."""
+	v = len(dist[0])
+	for k in range(v):
+		for i in range(v):
+			for j in range(v):
+				if dist[i][j] > dist[i][k] + dist[k][j]:
+					dist[i][j] = dist[i][k] + dist[k][j]
+	print("Shortest distances between every pair of vertices :")
+	for i in range(v):
+		for j in range(v):
+			if dist[i][j] == inf:
+				print("INF", end="  ")
+			else:
+				print(dist[i][j], end="  ")
+		print()
+
+graph = [
+	[0, inf, 3, inf],
+	[2, 0, inf, inf],
+	[inf, 6, 0, 1],
+	[5, inf, inf, 0]
+]
+all_pair_shortest_path(graph)
+
+# Success
