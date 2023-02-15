1f71dfabae002e585ecf46c0a2c3976ba65102f0
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2023-02-16 01:50:09 +0530

    update

--- quick_sort.py
+++ quick_sort.py
@@ -6,10 +6,10 @@ def quick_sort(arr, low, high):
 		quick_sort(arr, low, pivot - 1)
 		quick_sort(arr, pivot + 1, high)
 
-def partition(arr, pivot, high):
+def partition(arr, low, high):
 	pivot = arr[high]
-	i = pivot - 1
-	for j in range(pivot, high - 1):
+	i = low - 1
+	for j in range(low, high):
 		if arr[j] <= pivot:
 			i += 1
 			arr[i], arr[j] = arr[j], arr[i]
